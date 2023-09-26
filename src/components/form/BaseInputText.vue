aeeb398f960e3d7af3d79d124e57efe42a5b95dd
Dvortsov Andrey <exzy92@gmail.com>
2023-09-27 01:36:19 +0300

    Created input and select components

---
+++ src/components/form/BaseInputText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label :for="idName">{{ label }}</label>
+    <input
+      type="text"
+      :id="idName"
+      :value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    idName: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  border-radius: 5px;
+  border: 2px solid grey;
+  padding: 5px;
+  width: 70%;
+  outline: none;
+}
+</style>
---
+++ src/components/form/BaseSelectUsers.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <label :for="idName">{{ label }}</label>
+    <select
+      :id="idName"
+      :value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+      <option value="" selected></option>
+      <option v-for="user in users" :value="user.name" :key="user.id">
+        {{ user.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    idName: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      users: JSON.parse(localStorage.getItem('users')) || [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+select {
+  border-radius: 5px;
+  border: 2px solid grey;
+  padding: 5px;
+  width: 70%;
+  outline: none;
+}
+</style>
